e0a12d68ff738d2e28d6e6f45e16c0a61413fd53
Silvan Stöckli <silvanstoeckli05@gmail.com>
2023-09-07 14:28:58 +0200

    WebAPI

---
+++ QuickOrderSystemWebAPI/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuickOrderSystemWebAPI.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
