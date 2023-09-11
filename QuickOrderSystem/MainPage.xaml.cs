a342f78502e2e55cd5f731e293538686837e9cc7
SilverCoin007 <silvanstoeckli05@gmail.com>
2023-09-11 13:32:20 +0200

    Phone setup

--- QuickOrderSystem/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuickOrderSystem;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
--- QuickOrderSystem/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace QuickOrderSystem;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
